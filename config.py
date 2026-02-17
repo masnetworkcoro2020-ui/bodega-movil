35fad90ee05c3d9c123920fea865f93de44db02b
masnetworkcoro2020-ui <masnetworkcoro2020@gmail.com>
2026-02-17 18:21:01 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import streamlit as st
 from supabase import create_client
 
 def conectar():
-    # Usa las credenciales guardadas en Secrets de Streamlit Cloud
-    url = st.secrets["URL"]
-    key = st.secrets["KEY"]
-    return create_client(url, key)
+    # Llaves puestas directamente
+    URL = "https://aznkqqrakzhvbtlnjaxz.supabase.co"
+    KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6bmtxcXJha3podmJ0bG5qYXh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NjY4NTAsImV4cCI6MjA4NTU0Mjg1MH0.4LRC-DsHidHkYyS4CiLUy51r-_lEgGPMvKL7_DnJWFI"
+    return create_client(URL, KEY)
